bd53ee90f3cc24458fe5b47c9195ed2a5e8e79a9
Robert Schwartz <robertschwartz2@measinc.com>
2021-12-14 14:47:38 -0500

    typo fix; remove redundant writer def

--- lib/dotNet Trace Logging/VB.NET Trace Custom MachineNamedFileTraceListener.vb
+++ lib/dotNet Trace Logging/VB.NET Trace Custom MachineNamedFileTraceListener.vb
@@ -2,46 +2,46 @@ Imports System.Text
 
 Namespace SampleProgram
 
-	Public Class MachineNamedFileTraceListener
-		Inherits TextWriterTraceListener
+Public Class MachineNamedFileTraceListener
+    Inherits TextWriterTraceListener
 
-		Public Sub New(ByVal initializeData As String)
-			MyBase.New(initializeData.Replace(".log", "." & Environment.MachineName & ".log"))
-		End Sub
+    Public Sub New(ByVal initializeData As String)
+        MyBase.New(initializeData.Replace(".log", "." & Environment.MachineName & ".log"))
+    End Sub
 
-		Public Overrides Sub Write(ByVal message As String)
-			Dim fMessage = FormatMessage(message)
-			_traceWriter.Write(fMessage)
-		End Sub
+    Public Overrides Sub Write(ByVal message As String)
+        Dim fMessage = FormatMessage(message)
+        MyBase.Write(fMessage)
+    End Sub
 
-		Public Overrides Sub Write(ByVal message As String, ByVal category As String)
-			Dim fMessage = FormatMessage(message, category)
-			_traceWriter.Write(fMessage)
-		End Sub
+    Public Overrides Sub Write(ByVal message As String, ByVal category As String)
+        Dim fMessage = FormatMessage(message, category)
+        MyBase.Write(fMessage)
+    End Sub
 
-		Public Overrides Sub WriteLine(ByVal message As String)
-			Dim fMessage = FormatMessage(message)
-			_traceWriter.WriteLine(fMessage)
-		End Sub
+    Public Overrides Sub WriteLine(ByVal message As String)
+        Dim fMessage = FormatMessage(message)
+        MyBase.WriteLine(fMessage)
+    End Sub
 
-		Public Overrides Sub WriteLine(ByVal message As String, ByVal category As String)
-			Dim fMessage = FormatMessage(message, category)
-			_traceWriter.WriteLine(fMessage)
-		End Sub
+    Public Overrides Sub WriteLine(ByVal message As String, ByVal category As String)
+        Dim fMessage = FormatMessage(message, category)
+        MyBase.WriteLine(fMessage)
+    End Sub
 
-		Private Function FormatLogLine(ByVal message As String, ByVal category As String = "Information")
-			Dim sb As StringBuilder = New StringBuilder()
-			sb.Append(DateTime.Now)
-			sb.Append(vbTab)
-			sb.Append(Environment.MachineName)
-			sb.Append(vbTab)
-			sb.Append(category)
-			sb.Append(vbTab)
-			sb.Append(message)
-			Return sb.ToString()
-		End Function
+    Private Function FormatMessage(ByVal message As String, Optional ByVal category As String = "Information") As String
+        Dim sb As StringBuilder = New StringBuilder()
+        sb.Append(DateTime.Now)
+        sb.Append(vbTab)
+        sb.Append(Environment.MachineName)
+        sb.Append(vbTab)
+        sb.Append(category)
+        sb.Append(vbTab)
+        sb.Append(message)
+        Return sb.ToString()
+    End Function
 
-	End Class
+End Class
 
 ' SAMPLE CONFIG FILE SETUP
 
