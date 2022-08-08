3af2cc4e3f7434a23e32f57a449a0b32c2a44458
Robert Schwartz <robertschwartz2@measinc.com>
2022-08-08 02:34:02 -0400

    sample dataset select

---
+++ lib/SQL Select Sample Dataset of US Presidents.sql
@@ -0,0 +1,46 @@
+Select 1 as PresidentNumber, 'George Washington' as PresidentName
+Union Select 2 as PresidentNumber, 'John Adams' as PresidentName
+Union Select 3 as PresidentNumber, 'Thomas Jefferson' as PresidentName
+Union Select 4 as PresidentNumber, 'James Madison' as PresidentName
+Union Select 5 as PresidentNumber, 'James Monroe' as PresidentName
+Union Select 6 as PresidentNumber, 'John Quincy Adams' as PresidentName
+Union Select 7 as PresidentNumber, 'Andrew Jackson' as PresidentName
+Union Select 8 as PresidentNumber, 'Martin Van Buren' as PresidentName
+Union Select 9 as PresidentNumber, 'William Henry Harrison' as PresidentName
+Union Select 10 as PresidentNumber, 'John Tyler' as PresidentName
+Union Select 11 as PresidentNumber, 'James K. Polk' as PresidentName
+Union Select 12 as PresidentNumber, 'Zachary Taylor' as PresidentName
+Union Select 13 as PresidentNumber, 'Millard Fillmore' as PresidentName
+Union Select 14 as PresidentNumber, 'Franklin Pierce' as PresidentName
+Union Select 15 as PresidentNumber, 'James Buchanan' as PresidentName
+Union Select 16 as PresidentNumber, 'Abraham Lincoln' as PresidentName
+Union Select 17 as PresidentNumber, 'Andrew Johnson' as PresidentName
+Union Select 18 as PresidentNumber, 'Ulysses S. Grant' as PresidentName
+Union Select 19 as PresidentNumber, 'Rutherford B. Hayes' as PresidentName
+Union Select 20 as PresidentNumber, 'James A. Garfield' as PresidentName
+Union Select 21 as PresidentNumber, 'Chester A. Arthur' as PresidentName
+Union Select 22 as PresidentNumber, 'Grover Cleveland' as PresidentName
+Union Select 23 as PresidentNumber, 'Benjamin Harrison' as PresidentName
+Union Select 24 as PresidentNumber, 'Grover Cleveland' as PresidentName
+Union Select 25 as PresidentNumber, 'William McKinley' as PresidentName
+Union Select 26 as PresidentNumber, 'Theodore Roosevelt' as PresidentName
+Union Select 27 as PresidentNumber, 'William Howard Taft' as PresidentName
+Union Select 28 as PresidentNumber, 'Woodrow Wilson' as PresidentName
+Union Select 29 as PresidentNumber, 'Warren G. Harding' as PresidentName
+Union Select 30 as PresidentNumber, 'Calvin Coolidge' as PresidentName
+Union Select 31 as PresidentNumber, 'Herbert Hoover' as PresidentName
+Union Select 32 as PresidentNumber, 'Franklin D. Roosevelt' as PresidentName
+Union Select 33 as PresidentNumber, 'Harry S. Truman' as PresidentName
+Union Select 34 as PresidentNumber, 'Dwight D. Eisenhower' as PresidentName
+Union Select 35 as PresidentNumber, 'John F. Kennedy' as PresidentName
+Union Select 36 as PresidentNumber, 'Lyndon B. Johnson' as PresidentName
+Union Select 37 as PresidentNumber, 'Richard Nixon' as PresidentName
+Union Select 38 as PresidentNumber, 'Gerald Ford' as PresidentName
+Union Select 39 as PresidentNumber, 'Jimmy Carter' as PresidentName
+Union Select 40 as PresidentNumber, 'Ronald Reagan' as PresidentName
+Union Select 41 as PresidentNumber, 'George H. W. Bush' as PresidentName
+Union Select 42 as PresidentNumber, 'Bill Clinton' as PresidentName
+Union Select 43 as PresidentNumber, 'George W. Bush' as PresidentName
+Union Select 44 as PresidentNumber, 'Barack Obama' as PresidentName
+Union Select 45 as PresidentNumber, 'Donald J. Trump' as PresidentName
+Union Select 46 as PresidentNumber, 'Joe Biden' as PresidentName;
