a2f76fbe853e1b4bd191a9c0fc0018ce51aef945
Robert Schwartz <robertschwartz2@measinc.com>
2022-08-09 16:04:23 -0400

    output from linqpad

--- lib/SQL Select Sample Dataset of US Presidents.sql
+++ lib/SQL Select Sample Dataset of US Presidents.sql
@@ -1,3 +1,4 @@
+-- across then down ssrs lab
 Select 1 as [Page], 1 as [Col], 1 as [Row], 1 as PrezNum, 'George Washington' as PrezName
 Union Select 1 as [Page], 2 as [Col], 1 as [Row], 2 as PrezNum, 'John Adams' as PrezName
 Union Select 1 as [Page], 1 as [Col], 2 as [Row], 3 as PrezNum, 'Thomas Jefferson' as PrezName
@@ -12,36 +13,36 @@ Union Select 1 as [Page], 1 as [Col], 6 as [Row], 11 as PrezNum, 'James K. Polk'
 Union Select 1 as [Page], 2 as [Col], 6 as [Row], 12 as PrezNum, 'Zachary Taylor' as PrezName
 Union Select 1 as [Page], 1 as [Col], 7 as [Row], 13 as PrezNum, 'Millard Fillmore' as PrezName
 Union Select 1 as [Page], 2 as [Col], 7 as [Row], 14 as PrezNum, 'Franklin Pierce' as PrezName
-Union Select 1 as [Page], 1 as [Col], 8 as [Row], 15 as PrezNum, 'James Buchanan' as PrezName
-Union Select 1 as [Page], 2 as [Col], 8 as [Row], 16 as PrezNum, 'Abraham Lincoln' as PrezName
-Union Select 1 as [Page], 1 as [Col], 9 as [Row], 17 as PrezNum, 'Andrew Johnson' as PrezName
-Union Select 1 as [Page], 2 as [Col], 9 as [Row], 18 as PrezNum, 'Ulysses S. Grant' as PrezName
-Union Select 1 as [Page], 1 as [Col], 10 as [Row], 19 as PrezNum, 'Rutherford B. Hayes' as PrezName
-Union Select 1 as [Page], 2 as [Col], 10 as [Row], 20 as PrezNum, 'James A. Garfield' as PrezName
-Union Select 2 as [Page], 1 as [Col], 1 as [Row], 21 as PrezNum, 'Chester A. Arthur' as PrezName
-Union Select 2 as [Page], 2 as [Col], 1 as [Row], 22 as PrezNum, 'Grover Cleveland' as PrezName
-Union Select 2 as [Page], 1 as [Col], 2 as [Row], 23 as PrezNum, 'Benjamin Harrison' as PrezName
-Union Select 2 as [Page], 2 as [Col], 2 as [Row], 24 as PrezNum, 'Grover Cleveland' as PrezName
-Union Select 2 as [Page], 1 as [Col], 3 as [Row], 25 as PrezNum, 'William McKinley' as PrezName
-Union Select 2 as [Page], 2 as [Col], 3 as [Row], 26 as PrezNum, 'Theodore Roosevelt' as PrezName
-Union Select 2 as [Page], 1 as [Col], 4 as [Row], 27 as PrezNum, 'William Howard Taft' as PrezName
-Union Select 2 as [Page], 2 as [Col], 4 as [Row], 28 as PrezNum, 'Woodrow Wilson' as PrezName
-Union Select 2 as [Page], 1 as [Col], 5 as [Row], 29 as PrezNum, 'Warren G. Harding' as PrezName
-Union Select 2 as [Page], 2 as [Col], 5 as [Row], 30 as PrezNum, 'Calvin Coolidge' as PrezName
-Union Select 2 as [Page], 1 as [Col], 6 as [Row], 31 as PrezNum, 'Herbert Hoover' as PrezName
-Union Select 2 as [Page], 2 as [Col], 6 as [Row], 32 as PrezNum, 'Franklin D. Roosevelt' as PrezName
-Union Select 2 as [Page], 1 as [Col], 7 as [Row], 33 as PrezNum, 'Harry S. Truman' as PrezName
-Union Select 2 as [Page], 2 as [Col], 7 as [Row], 34 as PrezNum, 'Dwight D. Eisenhower' as PrezName
-Union Select 2 as [Page], 1 as [Col], 8 as [Row], 35 as PrezNum, 'John F. Kennedy' as PrezName
-Union Select 2 as [Page], 2 as [Col], 8 as [Row], 36 as PrezNum, 'Lyndon B. Johnson' as PrezName
-Union Select 2 as [Page], 1 as [Col], 9 as [Row], 37 as PrezNum, 'Richard Nixon' as PrezName
-Union Select 2 as [Page], 2 as [Col], 9 as [Row], 38 as PrezNum, 'Gerald Ford' as PrezName
-Union Select 2 as [Page], 1 as [Col], 10 as [Row], 39 as PrezNum, 'Jimmy Carter' as PrezName
-Union Select 2 as [Page], 2 as [Col], 10 as [Row], 40 as PrezNum, 'Ronald Reagan' as PrezName
-Union Select 3 as [Page], 1 as [Col], 1 as [Row], 41 as PrezNum, 'George H. W. Bush' as PrezName
-Union Select 3 as [Page], 2 as [Col], 1 as [Row], 42 as PrezNum, 'Bill Clinton' as PrezName
-Union Select 3 as [Page], 1 as [Col], 2 as [Row], 43 as PrezNum, 'George W. Bush' as PrezName
-Union Select 3 as [Page], 2 as [Col], 2 as [Row], 44 as PrezNum, 'Barack Obama' as PrezName
-Union Select 3 as [Page], 1 as [Col], 3 as [Row], 45 as PrezNum, 'Donald J. Trump' as PrezName
-Union Select 3 as [Page], 2 as [Col], 3 as [Row], 46 as PrezNum, 'Joe Biden' as PrezName
+Union Select 2 as [Page], 1 as [Col], 1 as [Row], 15 as PrezNum, 'James Buchanan' as PrezName
+Union Select 2 as [Page], 2 as [Col], 1 as [Row], 16 as PrezNum, 'Abraham Lincoln' as PrezName
+Union Select 2 as [Page], 1 as [Col], 2 as [Row], 17 as PrezNum, 'Andrew Johnson' as PrezName
+Union Select 2 as [Page], 2 as [Col], 2 as [Row], 18 as PrezNum, 'Ulysses S. Grant' as PrezName
+Union Select 2 as [Page], 1 as [Col], 3 as [Row], 19 as PrezNum, 'Rutherford B. Hayes' as PrezName
+Union Select 2 as [Page], 2 as [Col], 3 as [Row], 20 as PrezNum, 'James A. Garfield' as PrezName
+Union Select 2 as [Page], 1 as [Col], 4 as [Row], 21 as PrezNum, 'Chester A. Arthur' as PrezName
+Union Select 2 as [Page], 2 as [Col], 4 as [Row], 22 as PrezNum, 'Grover Cleveland' as PrezName
+Union Select 2 as [Page], 1 as [Col], 5 as [Row], 23 as PrezNum, 'Benjamin Harrison' as PrezName
+Union Select 2 as [Page], 2 as [Col], 5 as [Row], 24 as PrezNum, 'Grover Cleveland' as PrezName
+Union Select 2 as [Page], 1 as [Col], 6 as [Row], 25 as PrezNum, 'William McKinley' as PrezName
+Union Select 2 as [Page], 2 as [Col], 6 as [Row], 26 as PrezNum, 'Theodore Roosevelt' as PrezName
+Union Select 2 as [Page], 1 as [Col], 7 as [Row], 27 as PrezNum, 'William Howard Taft' as PrezName
+Union Select 2 as [Page], 2 as [Col], 7 as [Row], 28 as PrezNum, 'Woodrow Wilson' as PrezName
+Union Select 3 as [Page], 1 as [Col], 1 as [Row], 29 as PrezNum, 'Warren G. Harding' as PrezName
+Union Select 3 as [Page], 2 as [Col], 1 as [Row], 30 as PrezNum, 'Calvin Coolidge' as PrezName
+Union Select 3 as [Page], 1 as [Col], 2 as [Row], 31 as PrezNum, 'Herbert Hoover' as PrezName
+Union Select 3 as [Page], 2 as [Col], 2 as [Row], 32 as PrezNum, 'Franklin D. Roosevelt' as PrezName
+Union Select 3 as [Page], 1 as [Col], 3 as [Row], 33 as PrezNum, 'Harry S. Truman' as PrezName
+Union Select 3 as [Page], 2 as [Col], 3 as [Row], 34 as PrezNum, 'Dwight D. Eisenhower' as PrezName
+Union Select 3 as [Page], 1 as [Col], 4 as [Row], 35 as PrezNum, 'John F. Kennedy' as PrezName
+Union Select 3 as [Page], 2 as [Col], 4 as [Row], 36 as PrezNum, 'Lyndon B. Johnson' as PrezName
+Union Select 3 as [Page], 1 as [Col], 5 as [Row], 37 as PrezNum, 'Richard Nixon' as PrezName
+Union Select 3 as [Page], 2 as [Col], 5 as [Row], 38 as PrezNum, 'Gerald Ford' as PrezName
+Union Select 3 as [Page], 1 as [Col], 6 as [Row], 39 as PrezNum, 'Jimmy Carter' as PrezName
+Union Select 3 as [Page], 2 as [Col], 6 as [Row], 40 as PrezNum, 'Ronald Reagan' as PrezName
+Union Select 3 as [Page], 1 as [Col], 7 as [Row], 41 as PrezNum, 'George H. W. Bush' as PrezName
+Union Select 3 as [Page], 2 as [Col], 7 as [Row], 42 as PrezNum, 'Bill Clinton' as PrezName
+Union Select 4 as [Page], 1 as [Col], 1 as [Row], 43 as PrezNum, 'George W. Bush' as PrezName
+Union Select 4 as [Page], 2 as [Col], 1 as [Row], 44 as PrezNum, 'Barack Obama' as PrezName
+Union Select 4 as [Page], 1 as [Col], 2 as [Row], 45 as PrezNum, 'Donald J. Trump' as PrezName
+Union Select 4 as [Page], 2 as [Col], 2 as [Row], 46 as PrezNum, 'Joe Biden' as PrezName
 Order By PrezNum;
