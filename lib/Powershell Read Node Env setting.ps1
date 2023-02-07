d03e6cfc6b2b04feb63b9ce72ce55780acccba93
Robert Schwartz <robertschwartz2@measinc.com>
2023-02-07 12:03:45 -0500

    fixed bug in env parsing. more complete example

--- lib/Powershell Read Node Env setting.ps1
+++ lib/Powershell Read Node Env setting.ps1
@@ -5,19 +5,50 @@ function Get-EnvSetting {
 		$SettingName
     )
 
-	$Settings = Select-String -Path $FilePath -Pattern $SettingName
-	if ($Settings.Matches.Count -lt 1) {
-		Return ''
-	}
+	$SearchPattern = "^$SettingName="
+	$Settings = Select-String -LiteralPath $FilePath -Pattern $SearchPattern
 	$SettingLine = $Settings.Line
+	if ($SettingLine.Length -lt 1) { Return '' }
 	$SettingLineParts = $SettingLine.Split('=')
 	$QuotedValue = $SettingLineParts[$SettingLineParts.Count-1]
 	$Value = $QuotedValue.Replace('"','')
-
 	Return $Value
 
 }
 
-Get-EnvSetting '..\.env' 'LOG'
-Get-EnvSetting '..\.env' 'remoteSourcePort'
-Get-EnvSetting '..\.env' 'nonExisiting'
+$env = "..\.env"
+
+"**********************"
+"Parsing file '$env'"
+""
+
+"Parsed .env key: ENVIRONMENT"
+Get-EnvSetting $env 'ENVIRONMENT'
+""
+
+"Parsed .env key: HOST"
+Get-EnvSetting $env 'HOST'
+""
+
+"Parsed .env key: NONEXISTING"
+Get-EnvSetting $env 'NONEXISTING'
+""
+
+"Parsed .env key: LOG"
+Get-EnvSetting $env 'LOG'
+""
+
+"Parsed .env key: SUPERLOG"
+Get-EnvSetting $env 'SUPERLOG'
+""
+
+"Done"
+"**********************"
+
+# # Sample .ENV lines
+# # Copy below .env file. Remove the first two chars "# " on every line
+# ENVIRONMENT="Debug"
+# HOST="MyComputer"
+# LOG=".\resources-temp\App-Log-yyyyMMddHHmmss.log"
+# # Super Log has same messages as Log, but appends each new run without rolling over between runs or dates
+# SUPERLOG=".\resources-temp\App-Log.log"
