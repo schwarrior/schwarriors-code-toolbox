53cfdaadb8eb15b5df8b5c6ccd73a333adad6fc3
Robert Schwartz <robertschwartz2@measinc.com>
2023-02-07 12:40:04 -0500

    log to mulitple files. improve slug encoding

--- lib/Powershell Slug Encode Remove Incompatible Path Unc and Url Uri Characters.ps1
+++ lib/Powershell Slug Encode Remove Incompatible Path Unc and Url Uri Characters.ps1
@@ -2,9 +2,11 @@ Function Get-Slug-Encoding {
     param ($InString)
 
     $InvalidPathChars = [IO.Path]::GetInvalidFileNameChars() -join ""
-    $SlugOutChars = $InvalidPathChars + ":. "
+    $SlugOutChars = $InvalidPathChars + ":. '()="
     $RegExp = "[{0}]" -f [RegEx]::Escape($SlugOutChars)
-    $OutString = $InString -replace $RegExp
+    $OutString = $InString -replace $RegExp, "-"
     $OutString = $OutString -replace "-+", "-"
+	$OutString = $OutString -replace "^-", ""
+	$OutString = $OutString -replace "-$", ""
     return $OutString
 }
--- lib/Powershell copy console out copy to log file.ps1
+++ lib/Powershell copy console out copy to log file.ps1
@@ -2,10 +2,12 @@ Function Get-Slug-Encoding {
     param ($InString)
 
     $InvalidPathChars = [IO.Path]::GetInvalidFileNameChars() -join ""
-    $SlugOutChars = $InvalidPathChars + ":. "
+    $SlugOutChars = $InvalidPathChars + ":. '()="
     $RegExp = "[{0}]" -f [RegEx]::Escape($SlugOutChars)
-    $OutString = $InString -replace $RegExp
+    $OutString = $InString -replace $RegExp, "-"
     $OutString = $OutString -replace "-+", "-"
+	$OutString = $OutString -replace "^-", ""
+	$OutString = $OutString -replace "-$", ""
     return $OutString
 }
 
@@ -16,10 +18,20 @@ $RootPath = Resolve-Path -Path ".\"
 $LogFile = "$($processForFileName)-$($RunDate).log"
 $LogPath = Join-Path -Path $RootPath -ChildPath $LogFile
 
+$SuperLogFile = "$($processForFileName).log"
+$SuperLogPath = Join-Path -Path $RootPath -ChildPath $SuperLogFile
+
 Write-Host "Starting $($process)"
 Write-Host "Copying all console output to $($LogPath)"
+Write-Host "Also appending same console output to $($SuperLogPath)"
 
-# node .\index.js > "$($LogPath)"
-# 2>&1 redirects messages and errors into standard out ( '>' only ignores errors)
-# Tee-Object copies out to file while maintaining console out
+# Example of starting node program
+# Sending out to 1 log file and console. Includes errors
 Invoke-Expression $process 2>&1 | Tee-Object -file "$($LogPath)"
+
+# Example of starting node program
+# Sending output to 2 log files and console. Includes errors
+# Appends to $SuperLogFile. Overwites $LogFile
+Invoke-Expression $process 2>&1 | Tee-Object -Append -FilePath $SuperLogPath | Tee-Object -FilePath $LogPath
+# Direct version
+# node -e 'console.log(123)' 2>&1 | Tee-Object -Append -FilePath $SuperLogPath | Tee-Object -FilePath $LogPath
